6b833d946e9492cf17811070136312accdcd3fe4
Dax <daaawx@gmail.com>
2020-08-09 16:41:11 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test Amplify
+
+A test project using amplify-cli + react to deploy a serverless AWS application.
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {AmplifySignOut, withAuthenticator} from '@aws-amplify/ui-react';
+
+function App() {
+  return (
+      <div>
+        <h1>Hello from AWS Amplify</h1>
+        <AmplifySignOut/>
+      </div>
+  );
+}
+
+export default withAuthenticator(App);
