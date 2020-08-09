9ba607d02c4586e9a776b84794fbf2e6c860fb53
Dax <daaawx@gmail.com>
2020-08-09 17:02:33 +0200

    Create coins API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {API} from '@aws-amplify/api/lib/API';
 
 function App() {
+  const [coins, updateCoins] = useState([]);
+
+  // Function to call API
+  async function fetchCoins() {
+    const data = await API.get('cryptoapi', '/coins');
+    updateCoins(data.coins);
+  }
+
+  // Call fetchCoins when component loads
+  useEffect(() => {
+    fetchCoins();
+  }, []);
+
   return (
-      <div>
-        <h1>Hello from AWS Amplify</h1>
+      <div className="App">
+        {
+          coins.map((coin, index) =>
+              <div key={index}>
+                <h2>{coin.name} - {coin.symbol}</h2>
+                <h5>{coin.price_usd}</h5>
+              </div>,
+          )
+        }
       </div>
   );
 }
